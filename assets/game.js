1b0c4173bf5be9c76e6d54f2ee8bfda1a6729036
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2013-04-03 17:40:46 -0400

    Part 2 commit.

--- assets/game.js
+++ assets/game.js
@@ -1,26 +1,57 @@
-window.onload = function() {
-	// Check if rot.js can work on this browser
-	if (!ROT.isSupported()) {
-		alert("The rot.js library isn't supported by your browser.");
-	} else {
-		// Create a display 80 characters wide and 20 characters tall
-		var display = new ROT.Display({width:80, height:20});
-		var container = display.getContainer();
-		// Add the container to our HTML page
-		document.body.appendChild(container);
-		var foreground, background, colors;
-		for (var i = 0; i < 15; i++) {
-			// Calculate the foreground color, getting progressively darker
-			// and the background color, getting progressively lighter.
-			foreground = ROT.Color.toRGB([255 - (i*20),
-			                              255 - (i*20),
-			                              255 - (i*20)]);
-			background = ROT.Color.toRGB([i*20, i*20, i*20]);
-			// Create the color format specifier.
-			colors = "%c{" + foreground + "}%b{" + background + "}";
-			// Draw the text two columns in and at the row specified
-			// by i
-			display.drawText(2, i, colors + "Hello, world!");
-		}
+var Game =  {
+	_display: null,
+	_currentScreen: null,
+	init: function() {
+	    // Any necessary initialization will go here.
+	    this._display = new ROT.Display({width: 80, height: 24});
+	    // Create a helper function for binding to an event
+	    // and making it send it to the screen
+	    var game = this; // So that we don't lose this
+	    var bindEventToScreen = function(event) {
+	        window.addEventListener(event, function(e) {
+	            // When an event is received, send it to the
+	            // screen if there is one
+	            if (game._currentScreen !== null) {
+	                // Send the event type and data to the screen
+	                game._currentScreen.handleInput(event, e);
+	            }
+	        });
+	    }
+	    // Bind keyboard input events
+	    bindEventToScreen('keydown');
+	    bindEventToScreen('keyup');
+	    bindEventToScreen('keypress');
+	},
+	getDisplay: function() {
+		return this._display;
+	},
+	switchScreen: function(screen) {
+	    // If we had a screen before, notify it that we exited
+	    if (this._currentScreen !== null) {
+	        this._currentScreen.exit();
+	    }
+	    // Clear the display
+	    this.getDisplay().clear();
+	    // Update our current screen, notify it we entered
+	    // and then render it
+	    this._currentScreen = screen;
+	    if (!this._currentScreen !== null) {
+	        this._currentScreen.enter();
+	        this._currentScreen.render(this._display);
+	    }
 	}
+}
+
+window.onload = function() {
+    // Check if rot.js can work on this browser
+    if (!ROT.isSupported()) {
+        alert("The rot.js library isn't supported by your browser.");
+    } else {
+        // Initialize the game
+        Game.init();
+        // Add the container to our HTML page
+        document.body.appendChild(Game.getDisplay().getContainer());
+        // Load the start screen
+        Game.switchScreen(Game.Screen.startScreen);
+    }
 }
---
+++ assets/screens.js
@@ -0,0 +1,76 @@
+Game.Screen = {};
+
+// Define our initial start screen
+Game.Screen.startScreen = {
+    enter: function() {    console.log("Entered start screen."); },
+    exit: function() { console.log("Exited start screen."); },
+    render: function(display) {
+        // Render our prompt to the screen
+        display.drawText(1,1, "%c{yellow}Javascript Roguelike");
+        display.drawText(1,2, "Press [Enter] to start!");
+    },
+    handleInput: function(inputType, inputData) {
+        // When [Enter] is pressed, go to the play screen
+        if (inputType === 'keydown') {
+            if (inputData.keyCode === ROT.VK_RETURN) {
+                Game.switchScreen(Game.Screen.playScreen);
+            }
+        }
+    }
+}
+
+// Define our playing screen
+Game.Screen.playScreen = {
+    enter: function() {    console.log("Entered play screen."); },
+    exit: function() { console.log("Exited play screen."); },
+    render: function(display) {
+        display.drawText(3,5, "%c{red}%b{white}This game is so much fun!");
+        display.drawText(4,6, "Press [Enter] to win, or [Esc] to lose!");
+    },
+    handleInput: function(inputType, inputData) {
+        if (inputType === 'keydown') {
+            // If enter is pressed, go to the win screen
+            // If escape is pressed, go to lose screen
+            if (inputData.keyCode === ROT.VK_RETURN) {
+                Game.switchScreen(Game.Screen.winScreen);
+            } else if (inputData.keyCode === ROT.VK_ESCAPE) {
+                Game.switchScreen(Game.Screen.loseScreen);
+            }
+        }    
+    }
+}
+
+// Define our winning screen
+Game.Screen.winScreen = {
+    enter: function() {    console.log("Entered win screen."); },
+    exit: function() { console.log("Exited win screen."); },
+    render: function(display) {
+        // Render our prompt to the screen
+        for (var i = 0; i < 22; i++) {
+            // Generate random background colors
+            var r = Math.round(Math.random() * 255);
+            var g = Math.round(Math.random() * 255);
+            var b = Math.round(Math.random() * 255);
+            var background = ROT.Color.toRGB([r, g, b]);
+            display.drawText(2, i + 1, "%b{" + background + "}You win!");
+        }
+    },
+    handleInput: function(inputType, inputData) {
+        // Nothing to do here      
+    }
+}
+
+// Define our winning screen
+Game.Screen.loseScreen = {
+    enter: function() {    console.log("Entered lose screen."); },
+    exit: function() { console.log("Exited lose screen."); },
+    render: function(display) {
+        // Render our prompt to the screen
+        for (var i = 0; i < 22; i++) {
+            display.drawText(2, i + 1, "%b{red}You lose! :(");
+        }
+    },
+    handleInput: function(inputType, inputData) {
+        // Nothing to do here      
+    }
+}
